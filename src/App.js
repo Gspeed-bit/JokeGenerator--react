935e9ed677285e1ad558aca34a42496c141c3ea8
Your Name <adeyeye17@gmail.com>
2021-09-28 20:29:46 +0200

    UseRandomJoke component was added to take care of the API datausing fetch and custom hook was also used

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 
 import './App.css'
-//import UseRandomJoke from './UseRandomJoke'
+import UseRandomJoke from './UseRandomJoke'
 
 export default function App() {
 
- 
-
+   const joke = UseRandomJoke("daniel","martha");
 return (
-    <div>
+  <div>
       <form>
         <center>
+          <h2>{joke}</h2>
+     
           <input type="text"
           
           />
+
           <button>Generate Joke</button>
         </center>
       </form>
--- src/UseRandomJoke.js
+++ src/UseRandomJoke.js
@@ -1,9 +1,21 @@
-import React from 'react'
 
-export default function UseRandomJoke() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { useState, useEffect} from "react"
+
+export default function UseRandomJoke(firstName, lastName) {
+  const [joke, setJoke] = useState("")
+
+  useEffect(()=>{
+    const fetchJoke = async () =>{
+    await fetch(`http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`)
+    .then(res => res.json())
+      .then(data => {
+        setJoke(data.value.joke)
+      })
+    }
+    fetchJoke();
+    
+  },[firstName,lastName])
+  
+
+  return joke;
+  }
