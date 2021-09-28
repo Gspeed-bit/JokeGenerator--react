64ba90f241f9ae23b87dd0b8676a1d6e92aaf28b
Your Name <adeyeye17@gmail.com>
2021-09-28 20:42:01 +0200

    Two input text was creted was pass in the firstname and lastname to the joke displayed and useState was also used to set the values of the inputed names

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import {useState}  from "react"
 import './App.css'
 import UseRandomJoke from './UseRandomJoke'
 
 export default function App() {
 
+const [firstName, setFirstName] = useState("")
+const [lastName, setLastName] = useState("")
+
    const joke = UseRandomJoke("daniel","martha");
+
+
+
+
+   function jokeGenerator(event){
+    event.preventDefault();
+  
+   }
+
 return (
   <div>
       <form>
@@ -12,10 +24,20 @@ return (
           <h2>{joke}</h2>
      
           <input type="text"
-          
+          name="firstName"
+          value={firstName}
+          placeholder="First Name"
+          onChange={(e)=>setFirstName(e.target.value)}
+          />
+     
+          <input type="text"
+          name="lastName"
+          value={lastName}
+          placeholder="Last Name"
+          onChange={(e)=>setLastName(e.target.value)}
           />
 
-          <button>Generate Joke</button>
+          <button onClick={jokeGenerator}>Generate Joke</button>
         </center>
       </form>
      
